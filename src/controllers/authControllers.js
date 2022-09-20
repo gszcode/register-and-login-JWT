6a2051a813075fe26a973310bdbf31a4b9d66b2c
Gabriel <sgabrielok01@gmail.com>
2022-09-20 03:02:50 -0300

    controllador de login creado

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,22 +1,37 @@
 import User from "../models/User.js";
 
-// register user
+// register users
 export const register = async (req, res) => {
   const { first_name, last_name, email, password } = req.body;
-  console.log(req.body);
 
   try {
     let user = await User.findOne({ email });
-    console.log("USERRRR", user);
 
-    if (user) {
-      return res.status(404).json({ message: "Email ya existente" });
-    }
+    if (user) return res.status(404).json({ message: "Email ya existente" });
 
     user = new User({ first_name, last_name, email, password });
     await user.save();
 
     return res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// login users
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(404).json({ message: "Email no existente" });
+
+    const responsePassword = await user.comparePassword(password);
+    if (!responsePassword)
+      res.status(404).json({ message: "Contraseña incorrecta" });
+
+    return res.json({ message: "Logueado" });
   } catch (error) {
     console.log(error);
   }
